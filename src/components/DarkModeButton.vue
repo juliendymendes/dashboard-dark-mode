fb929b0063e5cd2ac7eb00c0bab9ff7e48dd9ca7
Juliendy Mendes <juliendy0708@gmail.com>
2022-12-03 18:00:24 -0400

    ui: dark mode component

---
+++ src/components/DarkModeButton.vue
@@ -0,0 +1,26 @@
+<template>
+	
+	<div class="d-flex">
+		<span class="dark-mode">Dark mode</span>
+		<div class="form-switch">
+			<input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+		</div>
+	</div>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+
+.dark-mode{
+	font-family: 'Inter-Bold', sans-serif;
+	color: hsl(228, 12%, 44%);
+	margin-right: 1rem;
+}
+
+
+</style>
