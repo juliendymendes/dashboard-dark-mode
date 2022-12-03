df957876d457d983dda764e0db6ab3afc873f8a4
Juliendy Mendes <juliendy0708@gmail.com>
2022-12-03 14:42:50 -0400

    setup: project setup

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<header>
+		<h1>Social Media Dashboard</h1>
+
+		<span>Total Followers: 23,004</span>
+
+		<span>Dark mode</span>
+		<button>On/Off</button>
+	</header>
+
+	<main>
+		<section>
+			<div>
+				<div>
+					<img src="..." alt="Facebook icon">
+					<span>@nathanf</span>
+				</div>
+	
+				<div>
+					1987 followers
+				</div>
+	
+				<div>
+					<img src="..." alt="Up arrow">
+					<span>12 Today</span>
+				</div>
+			</div>
+
+		</section>
+
+		<section>
+			<h2>Overview - Today</h2>
+
+			<div>
+				<div>
+					<span>Page views</span>
+					<img src="..." alt="Facebook icon">
+				</div>
+
+				<p>87</p>
+				<div>
+					<img src="..." alt="Up arrow">
+					<span>3%</span>
+				</div>
+			</div>
+		</section>
+	</main>
+
+	
+</template>
+
+<style scoped>
+
+</style>
